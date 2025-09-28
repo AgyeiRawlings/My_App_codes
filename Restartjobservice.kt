452537ed689b655b5da612d86132071b94310c68
Agyei <agyeirawlings35@gmail.com>
2025-09-28 11:06:43 +0000

    Update Restartjobservice.kt

--- Restartjobservice.kt
+++ Restartjobservice.kt
@@ -1,13 +1,16 @@
+package com.example.socketclient
+
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Intent
+import android.os.Build
 
 class RestartJobService : JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         // Restart your SocketService here
         val intent = Intent(this, SocketService::class.java)
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             startForegroundService(intent)
         } else {
             startService(intent)

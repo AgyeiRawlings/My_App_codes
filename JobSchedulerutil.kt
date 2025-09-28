2c6fcd3074f96bf5a6fab40ccc1a06a3894497e0
Agyei <agyeirawlings35@gmail.com>
2025-09-28 11:03:32 +0000

    Update JobSchedulerutil.kt

--- JobSchedulerutil.kt
+++ JobSchedulerutil.kt
@@ -1,16 +1,20 @@
+package com.example.socketclient
+
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Context
-import android.os.Build
 
-fun scheduleRestartJob() {
-    val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
-    val jobInfo = JobInfo.Builder(1, ComponentName(this, RestartJobService::class.java))
-        .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-        .setPersisted(true)
-        .setBackoffCriteria(1000, JobInfo.BACKOFF_POLICY_LINEAR)
-        .build()
+object JobSchedulerUtil {
+    
+    fun scheduleRestartJob(context: Context) {
+        val jobScheduler = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        val jobInfo = JobInfo.Builder(1, ComponentName(context, RestartJobService::class.java))
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+            .setPersisted(true)
+            .setBackoffCriteria(1000, JobInfo.BACKOFF_POLICY_LINEAR)
+            .build()
 
-    jobScheduler.schedule(jobInfo)
+        jobScheduler.schedule(jobInfo)
+    }
 }
